5ace8dcaeb9a82ee47b2673855deee20d41ff39e
PRIYANKA SINGH <priyankasingh1012ps@gmail.com>
2024-01-23 01:25:38 +0530

    frontend library 1

--- src/Dashboard/UploadBook.jsx
+++ src/Dashboard/UploadBook.jsx
@@ -55,7 +55,7 @@ const UploadBook = () => {
       bookPDFURL,
     };
     // console.log(dataObj)
-    fetch("http://localhost:5000/upload-book", {
+    fetch("https://e-library-jsw.onrender.com/upload-book", {
       method: "POST",
 
       headers: {
