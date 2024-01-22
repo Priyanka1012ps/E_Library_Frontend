c9eb78fc125ac88f98b0d486c41e03a5e08dfa44
PRIYANKA SINGH <priyankasingh1012ps@gmail.com>
2024-01-23 01:11:49 +0530

    frontend library 1

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCwScVfJDleadTx0JXLPSyMGW9fVlZYkSc",
+  authDomain: "mern-book-inventory-f0943.firebaseapp.com",
+  projectId: "mern-book-inventory-f0943",
+  storageBucket: "mern-book-inventory-f0943.appspot.com",
+  messagingSenderId: "713051289467",
+  appId: "1:713051289467:web:9e7fbcf9fee5f5b2976fde"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+
+export default app;
